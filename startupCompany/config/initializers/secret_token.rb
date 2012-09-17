8ddad6478f6f0cdf649d41a2fb73e8c6ea9a561c
Jessie Liu <jl4005@columbia.edu>
2012-09-17 00:10:17 -0400

    add rails project

---
+++ startupCompany/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StartupCompany::Application.config.secret_token = '3ea9e49dbacedc3dfc35adc49fa061d7adffb2b4bab641dfc6d7c0b9d4a58a108ed83633b1c3bc0b7bb9455a3087c953840244c012248bd9d43a3c5330b23ffb'
